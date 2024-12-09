6150608898973dea9901c398244fa5fb4b91a082
Isuru Fernando <isuruf@gmail.com>
2024-12-09 17:34:41 +0530

    disable static on windows too

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,19 +1,9 @@
 #!/usr/bin/env bash
 
-autoreconf -vfi
-chmod +x configure
-
-export CFLAGS="-g $CFLAGS"
-
-if [[ "$target_platform" == win-* ]]; then
-  ./configure --prefix="$PREFIX" --libdir="$PREFIX"/lib --disable-hardware-specific-code
-  patch_libtool
-else
-  ./configure --prefix="$PREFIX" --libdir="$PREFIX"/lib --disable-hardware-specific-code --disable-static
-fi
+./configure --prefix="$PREFIX" --libdir="$PREFIX"/lib --disable-hardware-specific-code --disable-static || cat config.log
 
 make -j${CPU_COUNT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-make check -j${CPU_COUNT}
+  make check -j${CPU_COUNT}
 fi
 make install
