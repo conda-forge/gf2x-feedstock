519a6dfc1a14b511037648484659275eef9ea22b
Isuru Fernando <isuruf@gmail.com>
2024-12-09 17:58:40 +0530

    fix testing

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ chmod +x configure
 ./configure --prefix="$PREFIX" --libdir="$PREFIX"/lib --disable-hardware-specific-code --disable-static || cat config.log
 
 make -j${CPU_COUNT}
-if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" && "${CROSSCOMPILING_EMULATOR:-}" == "" ]]; then
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
   make check -j${CPU_COUNT}
 fi
 make install
