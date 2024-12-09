8c6600257d378844ff3f50f2a872f155318ea801
Isuru Fernando <isuruf@gmail.com>
2024-12-09 19:00:49 +0530

    patch libtool

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,8 @@ chmod +x configure
 
 ./configure --prefix="$PREFIX" --libdir="$PREFIX"/lib --disable-hardware-specific-code --disable-static || cat config.log
 
+[[ "$target_platform" == "win-64" ]] && patch_libtool
+
 make -j${CPU_COUNT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
   make check -j${CPU_COUNT}
